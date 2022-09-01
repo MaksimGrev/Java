9b1bb8206ac7d32c1f92ac995b4cc59f1c3f3c18
Maxim <Ziganenok@ya.ru>
2022-09-01 21:27:55 +0300

    Ferst test

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+/*
+        git init   - Подгружаем git в проект
+        git config --global user.name 'Maxim'   - Выполняется только 1 раз
+        git config --global user.email 'Ziganenok@ya.ru'   - Выполняется только 1 раз
+        git add src/*   - Добавить файлы
+        git add src/*.java   - Добавить файлы
+        git add -A   - Добавить все файлы проекта
+        git status   - Проверить статус файлов проекта
+        git commit -m "Комментарий"  - Закачать файлы в локальный репозиторий
+        git rm --cached src/Main.java   - Убрать из коммида файл
+
+
+*.java
+src/
+.idea/
+Git_Lesson.iml
+
+
+        ////// Ветвление /////
+        git branch название    - создать ветвь
+        git checkout название    - перейти в ветвь
+        git merge названние_ветки
+
+        git branch -M master    - название для основной ветки
+        git remote add origin https://github.com/MaksimGrev/Java.git    - Подключаемся к гитХабу
+        git push -u origin master     - выгружаем файлы
+        git remote    - проверить подключение
+        git remote remove origin    - отключиться от репозитория
+ */
+    }
+}
